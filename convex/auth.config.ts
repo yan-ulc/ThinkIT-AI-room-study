eb215be173fbda6e162c3dafb8cab17ea390bc55
yannn912 <muhammad7135@gmail.com>
2026-05-03 03:47:27 +0700

    feat: add several theme

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 const authConfig = {
   providers: [
     {
-      domain: "https://charming-wildcat-89.clerk.accounts.dev",
+      domain: process.env.CLERK_JWT_ISSUER_DOMAIN || "https://charming-wildcat-89.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
